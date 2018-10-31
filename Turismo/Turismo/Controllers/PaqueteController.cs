0cb953784040a95cfe3a5e21ae1a951d59e2d5d7
Perez Grados, Jonathan Gerardo <perezgradosj@gmail.com>
2018-10-31 18:14:11 -0500

    Primer commit

---
+++ Turismo/Turismo/Controllers/PaqueteController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Turismo.Bean;
+using Turismo.Models;
+
+namespace Turismo.Controllers
+{
+    public class PaqueteController : Controller
+    {
+        PaqueteModel model = new PaqueteModel();
+
+        // GET: Paquete
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult FindById(int idpaquete) {
+
+            Paquete x = model.FindById(idpaquete);
+
+            return Json(x);
+        }
+
+        [HttpPost]
+        public JsonResult LstByRegion(string region)
+        {
+
+            List<Paquete> x = model.LstByRegion(region);
+
+            return Json(x);
+        }
+    }
+}
---
+++ Turismo/Turismo/Scripts/js/ListPaquetes.js
@@ -0,0 +1,123 @@
+﻿$(document).ready(function (e) {
+
+    //Listado de anuncios
+    var listado = function (key, rs) {
+        var divPaquetes = $("#div-paquetes");
+        divPaquetes.append(
+            "<div class='col-md-4' style='border-style: solid;padding: 10px;border-color: #eaedef;border-width: 1px;background: #eaedef47;'>" +
+                "<img src='/Img/" + rs.foto1Name + "' class='img-thumbnail rounded mx-auto d-block'  style='width:323px;height:221px;' />" +
+                "<div class='row'>" +
+                    "<div class='col-md-12' align='center'>" +
+                        "<label>" + rs.titulo + "</label>" +
+                    "</div>" +
+                    "<div class='col-md-12' align='center'>" +
+                        "<label>Precio: S/. " + rs.precioHabDobleTripe + " por persona</label>" +
+                    "</div>" +
+                    "<div class='col-md-12' align='center'>" +
+                        "<label>Duración: " + rs.dias + " días y " + rs.noches + " noches</label>" +
+                    "</div>" +
+                    "<div class='col-md-12' align='center'>" +
+                        "<button class='btn btn-outline-info btn-block btnDetallePaquete' value='" + rs.id + "' data-toggle='modal' data-target='#DetallePaqueteModal' >Ver detalle >></button>"+
+                    "</div>"+
+                "</div>" +
+            "</div>"
+        );
+    };
+    var lstPaquetes = function () {        
+        $.ajax({
+            type: "POST",
+            url: "/Anuncio/LstAnuncios",
+            dataType: "json",
+            success: function (result) {
+                //console.log(result);
+                $.each(result, listado);
+            }
+        });
+    }
+    lstPaquetes();
+
+    var lstDestino = function () {
+        var selectDestino = $("#select-región");
+        $.ajax({
+            type: "POST",
+            url: "/Anuncio/LstDestinos",
+            dataType: "json",
+            success: function (result) {
+                //console.log(result);
+                selectDestino.append(
+                    "<option value='todos'>Listar todos</option>"
+                );
+                $.each(result, function (key, rs) {
+                    selectDestino.append(
+                        "<option value='" + rs.destino + "'>" + rs.destino + "</option>"
+                    );
+                });
+            }
+        });
+    }
+    lstDestino();
+
+    var filtrarPaquetesByRegion = function () {
+        var region = $("#select-región").val();
+        $("#div-paquetes").empty();
+        $.ajax({
+            type: "POST",
+            url: "/Paquete/LstByRegion",
+            data: { region: region },
+            dataType: "json",
+            success: function (result) {
+                console.log(result);
+                $.each(result, listado);
+            }
+        });
+    };
+    $("#select-región").change(filtrarPaquetesByRegion);
+
+    var DetallePaquete = function () {
+        var idpaquete = $(this).val();
+        $("#iditinerarios tbody").empty();
+        $("#idalimentacion tbody").empty();
+        $.ajax({
+            type: "POST",
+            url: "/Paquete/FindById",
+            data: { idpaquete: idpaquete },
+            dataType: "json",
+            success: function (result) {
+                console.log(result);
+                console.log(result.itinerarios);
+                var itinerarios = result.itinerarios;
+                $("#idfoto1").attr("src", "/Img/" + result.foto1Name);
+                $("#idfoto2").attr("src", "/Img/" + result.foto2Name);
+                $("#idfoto3").attr("src", "/Img/" + result.foto3Name);
+                $("#idfoto4").attr("src", "/Img/" + result.foto4Name);
+                $("#idfoto5").attr("src", "/Img/" + result.foto5Name);
+                $("#idprecio").text(result.precioHabDobleTripe);
+                $("#idtitulo").text(result.titulo);
+                $("#idregion").text(result.destino);
+                $("#idduracion").text(result.dias + " dias y " + result.noches + " noches");
+                $("#idpasajeros").text(result.minPasajeros);
+                $("#idanticipado").text(result.horasAnticipado + " horas");
+
+                $.each(itinerarios, function (key, rs)  {
+                    $("#iditinerarios tbody").append(
+                        "<tr>" +
+                        "<td>" + rs.dia + "</td>" +
+                        "<td>" + rs.descripcion + "</td>" +
+                        "</tr>"
+                    );
+                });
+
+                $.each(itinerarios, function (key, rs) {
+                    $("#idalimentacion tbody").append(
+                        "<tr>" +
+                        "<td>" + rs.dia + "</td>" +
+                        "<td>" + rs.alojamiento + "</td>" +
+                        "<td>" + rs.alimentacion + "</td>" +
+                        "</tr>"
+                    );
+                });
+            }
+        });
+    };
+    $("#div-paquetes").on("click", ".btnDetallePaquete", DetallePaquete);
+});
